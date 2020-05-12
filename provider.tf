0db1cf88d2ec0d4632735a2478125b4c3a84b51f
bfrench88 <benjamin.french@braze.com>
2020-05-12 14:10:56 +0100

    upgraded providers

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 // Azure RM provider
 
 provider "azurerm" {
-  version = "~> 1.32"
+  version = "~> 1.44"
 }
 
 // Random provider
 
 provider "random" {
-  version = "~> 2.0"
+  version = "~> 2.2"
 }
